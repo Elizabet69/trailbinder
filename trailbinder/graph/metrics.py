5735bb46bebd982873843291af947182723cc0c2
Elizabet69 <kataikingsukdar@gmail.com>
2026-02-02 16:10:18 +0000

    Touch up trailbinder/graph/metrics.py (no functional change)

--- trailbinder/graph/metrics.py
+++ trailbinder/graph/metrics.py
@@ -24,3 +24,5 @@ def top_by_out(g: Dict[str, List[str]], n: int = 5) -> List[Tuple[str, int]]:
 # autosave 2025-12-15T19:48:18.964014+00:00
 
 # autosave 2026-01-09T21:13:26.626720+00:00
+
+# autosave 2026-02-02T16:10:18.838311+00:00
